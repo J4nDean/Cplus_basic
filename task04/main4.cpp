069023bc9a11d834e8c0d833b6a26bbeb315958e
J4n <jandziekan333@gmail.com>
2023-08-21 14:34:24 +0200

    up06.2

--- task04/main4.cpp
+++ task04/main4.cpp
@@ -1,13 +1,32 @@
 #include <iostream>
 #include <iomanip> // formatting
 
-void showPeriod(int period);
+void showPeriod(int period){
+    int fromy = (period >> 25) & 127;
+    int fromm = (period >> 21) & 15;
+    int fromd = (period >> 16) & 31;
+    int toy = (period >> 9) & 127;
+    int tom = (period >> 5) & 15;
+    int tod = period & 31;
 
-int pack(int fromy, int fromm, int fromd,
-         int toy, int tom, int tod);
+    std::cout << std::setw(4) << std::setfill('0') << fromy + 2000 << '/'
+              << std::setw(2) << std::setfill('0') << fromm << '/'
+              << std::setw(2) << std::setfill('0') << fromd << '-'
+              << std::setw(4) << std::setfill('0') << toy + 2000 << '/'
+              << std::setw(2) << std::setfill('0') << tom << '/'
+              << std::setw(2) << std::setfill('0') << tod;
+
+
+}
+
+int pack(int fromy, int fromm, int fromd, int toy, int tom, int tod) {
+    fromy -= 2000;
+    toy -= 2000;
+    return(fromy << 25) | (fromm << 21) | (fromd << 16) | (toy << 9) | (tom << 5) | tod;
+}
 
 int main() {
-                    //   y           m          d        y       m         d
-    int period = pack(2000, 2, 3, 2127, 11, 29);
+    //   y           m          d        y       m         d
+    int period = pack(2000, 2, 3, 2030, 11, 29);
     showPeriod(period);
 }
